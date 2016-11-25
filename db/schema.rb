a684ffdc31f1ba81514147cf683db897b374254c
jmoreno <jmoreno.zn@gmail.com>
2016-11-25 19:42:19 +0100

    arreglo de la migracion en postgresql, espero...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121000334) do
+ActiveRecord::Schema.define(version: 20161123235008) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
