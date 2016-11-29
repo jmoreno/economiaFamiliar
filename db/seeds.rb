f50641ed6099e1b9fb44adfa2e6920a347e43b37
jmoreno <jmoreno.zn@gmail.com>
2016-11-29 09:56:58 +0100

    Demasiadas cosas: nuevo campo admin para la tabla de usuarios, pagina de bienvenida, algunas redirecciones y una pagina de login mas bonita

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.delete_all
+
 user = User.new
 user.email = 'jmoreno.zn@gmail.com'
 user.password = 'piopioqueyonohesido'
 user.password_confirmation = 'piopioqueyonohesido'
+user.admin = true
 user.save!
+
+user = User.new
+user.email = 'amreyg@hotmail.com'
+user.password = 'vayalioelmontepio'
+user.password_confirmation = 'vayalioelmontepio'
+user.admin = true
+user.save!
+
